903d8c5f44c74a7108eae78a4118c73230622858
loidev02112001 <loidev02112001@gmail.com>
2023-07-13 10:24:56 +0700

    setup-prettier-jsconfig

--- src/main.js
+++ src/main.js
@@ -1,14 +1,27 @@
-import './assets/main.css'
+import '@/assets/scss/globalStyle.scss';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { VueQueryPlugin } from 'vue-query';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from '@/router/';
 
-const app = createApp(App)
+const vueQueryPluginOptions = {
+  queryClientConfig: {
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false
+      }
+    }
+  }
+};
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+app.use(pinia);
+app.use(router);
+app.use(VueQueryPlugin, vueQueryPluginOptions);
+app.mount('#app');
